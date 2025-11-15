3752f749e01d0f5aa3cd6dda3e787b52159edbd8
Weslley <weslleysoaresribeiro1@gmail.com>
2025-11-15 00:21:51 -0300

    atualizacao

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ console.log(listaTarefa);
 function adicionarTarefa() {
     const tarefa = inputTarefa.value;
     const lista = document.createElement('li');
+    const btnDeletar = document.createElement('button'); // consertei o typo "bnt"
+    
     lista.textContent = tarefa;
-
+    btnDeletar.textContent = '❌';
+    
+    // Evento do botão
+    btnDeletar.addEventListener('click', function(){
+        lista.remove();
+    });
+    
+    // Adicionar botão dentro do <li>
+    lista.appendChild(btnDeletar);
+    
+    // Adicionar <li> na lista (SÓ UMA VEZ!)
     listaTarefa.appendChild(lista);
+    
+    // Limpar input (SÓ UMA VEZ!)
     inputTarefa.value = '';
 }
 
 btnAdicionar.addEventListener('click', adicionarTarefa);
-
-// PASSO 1: Selecionar...?
-const numero = document.getElementById('numero');
-const botao = document.getElementById('btnAumentar');
-
-// PASSO 2 a 5: Criar função
-function aumentar() {        // PASSO 3: Processar (+1)
-    let atualValor = numero.textContent;
-    let valor = parseInt (atualValor);// PASSO 4: Mostrar novo valor
-    let soma = valor + 1;
-    numero.textContent = soma;
-}
-
- botao.addEventListener('click', aumentar);// Conectar botão
