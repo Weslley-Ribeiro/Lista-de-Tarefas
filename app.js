99a0bb7a529e3061cbb606bf70a051069ee9fb84
Weslley <weslleysoaresribeiro1@gmail.com>
2025-11-16 19:48:47 -0300

    atualizacao do projeto

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inputTarefa = document.getElementById('input-tarefa');
 const btnAdicionar = document.getElementById('btn-adicionar');
 const listaTarefa = document.getElementById('lista-tarefa');
+const elementoDataHora = document.getElementById('data-hora');
+const btnTema = document.getElementById('btn-tema');
 
 console.log(inputTarefa);
 console.log(btnAdicionar);
@@ -9,7 +11,7 @@ console.log(listaTarefa);
 function adicionarTarefa() {
     const tarefa = inputTarefa.value;
     const lista = document.createElement('li');
-    const btnDeletar = document.createElement('button'); // consertei o typo "bnt"
+    const btnDeletar = document.createElement('button'); 
     
     lista.textContent = tarefa;
     btnDeletar.textContent = '❌';
@@ -17,6 +19,10 @@ function adicionarTarefa() {
     // Evento do botão
     btnDeletar.addEventListener('click', function(){
         lista.remove();
+    });
+
+    lista.addEventListener('click', function() {
+        lista.classList.toggle('concluida');
     });
     
     // Adicionar botão dentro do <li>
@@ -29,4 +35,38 @@ function adicionarTarefa() {
     inputTarefa.value = '';
 }
 
+function atualizarDataHora() {
+    const agora = new Date();
+    
+    const horas = agora.getHours();
+    const minutos = agora.getMinutes();
+    const dia = agora.getDate();
+    const mes = agora.getMonth() + 1;
+    const ano = agora.getFullYear();
+    const segundos = agora.getSeconds();
+    
+    const textoDataHora = `${dia}/${mes}/${ano} - ${horas}:${minutos}:${segundos}`;
+    
+    elementoDataHora.textContent = textoDataHora;
+}
+
+function trocarTema() {
+    // Alternar a classe 'tema-claro' no body
+    document.body.classList.toggle('tema-claro');
+    
+    // Trocar o emoji do botão
+    if (document.body.classList.contains('tema-claro')) {
+        btnTema.textContent ='☀️'; // tema claro = sol
+    } else {
+        btnTema.textContent ='🌙'; // tema escuro = lua
+    }
+}
+
+btnTema.addEventListener('click', trocarTema);
 btnAdicionar.addEventListener('click', adicionarTarefa);
+
+// Chamar imediatamente
+atualizarDataHora();
+
+// Chamar a cada 1 segundo (1000ms)
+setInterval(atualizarDataHora, 1000);
